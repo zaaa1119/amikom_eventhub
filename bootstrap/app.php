85abaf190fed2c5cc6958b8784b6c513057c0be9
zaaa1119 <mnzmnz890@gmail.com>
2026-07-27 13:35:00 +0700

    ?

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         Authenticate::redirectUsing(function (Request $request) {
             return route('admin.login');
         });
